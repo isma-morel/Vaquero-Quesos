2335cee7c7bb13607013f9c8b5e4f65f818364b9
Maxi Cortes <skreamozz@gmail.com>
2021-05-10 20:31:58 -0300

    se preparo todo para agregar a los vendedores

--- src/componentes/Login/Login.js
+++ src/componentes/Login/Login.js
@@ -7,14 +7,26 @@ const Login = ({ logo, LogSucces }) => {
   const history = useHistory();
   const [error, setError] = useState();
   const [isLoading, setLoading] = useState(false);
+  const [isVendedor, setIsVendedor] = useState(false);
+  const [clientes, setClientes] = useState([]);
   useEffect(() => {
     const auth = JSON.parse(localStorage.getItem("auth"));
     if (auth) {
-      auth.TipoCliente === "C"
-        ? history.push("/Lista")
-        : history.push("/Dashboard");
+      switch (auth.TipoCliente) {
+        case "C":
+          history.push("/Lista");
+          break;
+        case "V":
+          break;
+        case "S":
+          history.push("/Dashboard");
+          break;
+        default:
+          break;
+      }
     }
   }, []);
+  const pedirListaClientes = async () => {};
   const handleSubmit = (e) => {
     e.preventDefault();
     const { target } = e;
@@ -39,9 +51,18 @@ const Login = ({ logo, LogSucces }) => {
           JSON.stringify({ ...json, usuario: target[0].value })
         );
         LogSucces();
-        json.TipoCliente === "C"
-          ? history.push("/Lista")
-          : history.push("/Dashboard");
+        switch (json.TipoCliente) {
+          case "C":
+            history.push("/Lista");
+            break;
+          case "V":
+            setIsVendedor(true);
+            pedirListaClientes();
+            break;
+          case "S":
+            history.push("/Dashboard");
+            break;
+        }
       })
       .catch((err) => setError(err.message))
       .finally(() => setLoading(false));
@@ -51,27 +72,38 @@ const Login = ({ logo, LogSucces }) => {
     <div className="container">
       <img src={logo} alt="Logo Vaquero" />
       <hr />
-      <form onSubmit={handleSubmit} className="container">
-        <span className="error">{error}</span>
-        <input required className="usuario" type="text" placeholder="Usuario" />
-        <input
-          required
-          className="contraseña"
-          type="password"
-          placeholder="Contraseña"
-        />
-        <button disabled={isLoading} className="button">
-          ENVIAR
-        </button>
-        <small className="cuenta">
-          Si no tiene una cuenta puede comunicarse a <br />
-          <span>
-            <a className="email" href="mailto:ventas@quesosvaquero.com">
-              ventas@quesosvaquero.com
-            </a>
-          </span>
-        </small>
-      </form>
+      {isVendedor ? (
+        <div class="container">
+          <select name="Clientes"></select>
+        </div>
+      ) : (
+        <form onSubmit={handleSubmit} className="container">
+          <span className="error">{error}</span>
+          <input
+            required
+            className="usuario"
+            type="text"
+            placeholder="Usuario"
+          />
+          <input
+            required
+            className="contraseña"
+            type="password"
+            placeholder="Contraseña"
+          />
+          <button disabled={isLoading} className="button">
+            ENVIAR
+          </button>
+          <small className="cuenta">
+            Si no tiene una cuenta puede comunicarse a <br />
+            <span>
+              <a className="email" href="mailto:ventas@quesosvaquero.com">
+                ventas@quesosvaquero.com
+              </a>
+            </span>
+          </small>
+        </form>
+      )}
     </div>
   );
 };
