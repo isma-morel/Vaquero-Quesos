27a28066abad57e87e3f1ea82e0c3260dbc292ee
Maxi Cortes <skreamozz@gmail.com>
2021-05-28 23:25:23 -0300

    se comenzo el formulario de carga y modificacion de productos.

--- src/componentes/CargaProductos/CargaProductos.js
+++ src/componentes/CargaProductos/CargaProductos.js
@@ -7,8 +7,11 @@ import "./CargaProductos.css";
 const CargaProductos = () => {
   const [productos, setProductos] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [isEditOrAdd, setIsEditOrAdd] = useState(false);
+  const [productoSeleccionado, setProductoSeleccionado] = useState();
   const { push } = useHistory();
   const obtenerPedidos = async () => {
+    setIsLoading(true);
     try {
       const auth = JSON.parse(localStorage.getItem("auth"));
       if (!auth) return push("/");
@@ -35,14 +38,33 @@ const CargaProductos = () => {
     return obtenerPedidos();
   }, []);
 
+  const handleRowClick = (index) => (e) => {
+    setProductoSeleccionado(productos[index]);
+    setIsEditOrAdd(true);
+  };
+  const handleAddClick = (e) => {
+    setIsEditOrAdd(true);
+  };
+  const onCloseAddOrEdit = (e) => {
+    setIsEditOrAdd(false);
+    setProductoSeleccionado(null);
+  };
+
   return (
     <div className="carga-productos">
       {isLoading ? (
         <div className="spin"></div>
+      ) : isEditOrAdd ? (
+        <AddOrEdit
+          productoSeleccionado={productoSeleccionado}
+          onClose={onCloseAddOrEdit}
+        />
       ) : (
         <div className="contenedor-tabla">
           <div className="contenedor-cliente">
-            <button className="btn add">Nueva</button>
+            <button onClick={handleAddClick} className="btn add">
+              Nueva
+            </button>
           </div>
           <table className="tabla tabla-pedidos">
             <thead>
@@ -53,8 +75,11 @@ const CargaProductos = () => {
               </tr>
             </thead>
             <tbody>
-              {productos.map(({ Codigo, Descripcion, Presentacion }) => (
-                <tr onClick={() => alert("pene")}>
+              {productos.map(({ Codigo, Descripcion, Presentacion }, index) => (
+                <tr
+                  key={index}
+                  title="Presione el click para editar"
+                  onClick={handleRowClick(index)}>
                   <td>{Codigo}</td>
                   <td>{Descripcion}</td>
                   <td>{Presentacion}</td>
@@ -64,6 +89,95 @@ const CargaProductos = () => {
           </table>
         </div>
       )}
+    </div>
+  );
+};
+
+const AddOrEdit = ({ productoSeleccionado, onClose }) => {
+  return (
+    <div style={{ minHeight: "100%", width: "100%" }}>
+      <div
+        style={{
+          display: "flex",
+          width: "80%",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "2em",
+          margin: "auto",
+        }}>
+        <div
+          style={{
+            display: "flex",
+            overflow: "hidden",
+            flexDirection: "column",
+            gap: ".2em",
+          }}>
+          <img
+            style={{
+              width: "200px",
+              height: "200px",
+              borderRadius: ".2em",
+              margin: "auto",
+            }}
+            src="https://quesosvaquero.com/img/quesos/Parmesano-media-horma.jpg"
+            alt=""
+          />
+          <button style={{ width: "100px", margin: "auto" }}>Subir</button>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            alignSelf: "flex-start",
+            gap: "1em",
+          }}>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <span>Codigo</span>
+            <input type="text" />
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <span>Descripcion</span>
+            <input type="text" />
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <span>Presentacion</span>
+            <input type="text" />
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <span>Medida Principal</span>
+            <input type="text" />
+          </div>
+        </div>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "flex-start",
+            alignSelf: "flex-start",
+            gap: "1em",
+          }}>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <span>Peso Promedio</span>
+            <input type="text" />
+          </div>
+          <div style={{ display: "flex", flexDirection: "column" }}>
+            <span>Porcentaje de Desvio</span>
+            <input type="text" />
+          </div>
+        </div>
+        <div></div>
+      </div>
+      <div style={{ width: "100%", textAlign: "center", padding: "2em" }}>
+        <button
+          style={{ margin: "auto", marginRight: "1em" }}
+          onClick={onClose}>
+          Cancelar
+        </button>
+        <button style={{ margin: "auto" }}>Guardar</button>
+      </div>
     </div>
   );
 };
